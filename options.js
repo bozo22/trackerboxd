42fe6c2f3cbeceefc95daff9235452bba96a160f
Domonkos Nagy <75812823+bozo22@users.noreply.github.com>
2023-06-20 16:51:02 +0200

    Updated a few things. Fixes #4

--- options.js
+++ options.js
@@ -6,12 +6,12 @@ function saveOptions(e) {
       ncore: document.querySelector("#ncore").checked,
       knaben: document.querySelector("#knaben").checked,
       nyaa: document.querySelector("#nyaa").checked,
-      rarbg: document.querySelector("#rarbg").checked,
       piratebay: document.querySelector("#piratebay").checked,
       yts: document.querySelector("#yts").checked,
       ttsx: document.querySelector("#ttsx").checked,
       rutracker: document.querySelector("#rutracker").checked,
     });
+    window.close();
   }
   
 function restoreOptions() {
@@ -21,7 +21,6 @@ function restoreOptions() {
       document.querySelector("#ncore").checked = result.ncore;
       document.querySelector("#knaben").checked = result.knaben;
       document.querySelector("#nyaa").checked = result.nyaa;
-      document.querySelector("#rarbg").checked = result.rarbg;
       document.querySelector("#piratebay").checked = result.piratebay;
       document.querySelector("#yts").checked = result.yts;
       document.querySelector("#ttsx").checked = result.ttsx;
--- trackerboxd.js
+++ trackerboxd.js
@@ -7,16 +7,15 @@ let trackers = {
     knaben: {text: "Search Knaben", link: "https://knaben.net/search/?q=" + title + " " + year + "#Seeders"},
     ncore: {text: "Search ncore", link: "https://ncore.pro/torrents.php?mire=" + title + " " + year},
     nyaa: {text: "Search Nyaa", link: "https://nyaa.si/?f=0&c=0_0&q=" + title + " " + year + "&s=seeders&o=desc"},
-    rarbg: {text: "Search RARBG", link: "https://rarbg.to/torrents.php?search=" + title + " " + year + "&order=seeders&by=DESC"},
     piratebay: {text: "Search The Pirate Bay", link: "https://pirate-bays.net/search?q=" + title + " " + year},
     yts: {text: "Search YTS", link: "https://yts.mx/browse-movies/" + title + " " + year + "/all/all/0/seeds/0/all"},
-    ttsx: {text: "Search 1337x", link: "https://www.1377x.to/sort-search/" + title + " " + year + "/seeders/desc/1/"},
+    ttsx: {text: "Search 1337x", link: "https://www.1337x.to/sort-search/" + title + " " + year + "/seeders/desc/1/"},
     rutracker: {text: "Search RuTracker", link: "https://rutracker.org/forum/tracker.php?nm=" + title + " " + year}
 }
 
 chrome.storage.sync.get(null, function(result) {
 for (let i in result) {
-    if (result[i]) {
+    if (result[i] && trackers[i]) {
         var node = document.createElement('li');
         var link = document.createElement('a');
         link.appendChild(document.createTextNode(trackers[i].text));
